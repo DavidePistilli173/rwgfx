e17cd9d5403f280655b8e6769b4fb4bb814bcb18
DavidePistilli173 <davidepistilli173@gmail.com>
2023-09-24 12:24:40 +0200

    Add basig wgpu initialisation code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,214 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use winit::{
+    event::{self, *},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    clear_color: wgpu::Color,
+    logger: rwlog::sender::Logger,
+    // Window must be dropped after surface.
+    window: Window,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl State {
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        match event {
+            WindowEvent::CursorMoved { position, .. } => {
+                self.clear_color.g = position.x / self.size.width as f64;
+                self.clear_color.b = position.y / self.size.height as f64;
+                self.window.request_redraw();
+                true
+            }
+            _ => false,
+        }
     }
+
+    // Creating some of the wgpu types requires async code
+    async fn new(window: Window, logger: rwlog::sender::Logger) -> Result<Self, ()> {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            dx12_shader_compiler: Default::default(),
+        });
+
+        let surface = unsafe { instance.create_surface(&window) }.map_err(|err| {
+            rwlog::rel_err!(&logger, "Failed to create window surface: {err}.");
+            ()
+        })?;
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .ok_or_else(|| {
+                rwlog::rel_err!(&logger, "Failed to get compatible graphics device.");
+                ()
+            })?;
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .map_err(|err| {
+                rwlog::rel_err!(
+                    &logger,
+                    "Failed to create logical graphics device and queue: {err}."
+                );
+                ()
+            })?;
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(surface_capabilities.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+        };
+        surface.configure(&device, &config);
+
+        Ok(Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            clear_color: wgpu::Color {
+                r: 0.1,
+                g: 0.0,
+                b: 0.0,
+                a: 1.0,
+            },
+            logger,
+            size,
+        })
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn update(&mut self) {}
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+}
+
+pub async fn run(logger: rwlog::sender::Logger) {
+    env_logger::init(); // Necessary for wgpu error logging.
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap_or_else(|err| {
+            rwlog::rel_fatal!(&logger, "Failed to create window: {err}.");
+        });
+
+    let mut state = State::new(window, logger.clone())
+        .await
+        .unwrap_or_else(|err| {
+            rwlog::rel_fatal!(&logger, "Failed to initialise graphics state.");
+        });
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            window_id,
+            ref event,
+        } => {
+            if window_id == state.window().id() && !state.input(&event) {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => state.resize(*physical_size),
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size)
+                    }
+                    _ => (),
+                }
+            }
+        }
+        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => (),
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                Err(wgpu::SurfaceError::OutOfMemory) => {
+                    rwlog::rel_err!(&logger, "Not enough GPU memory!");
+                    *control_flow = ControlFlow::Exit;
+                }
+                Err(e) => {
+                    rwlog::warn!(&logger, "{e}");
+                }
+            };
+        }
+        Event::MainEventsCleared => {
+            state.window().request_redraw();
+        }
+        _ => (),
+    });
 }
